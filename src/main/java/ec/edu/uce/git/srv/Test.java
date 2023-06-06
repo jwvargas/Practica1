87d9418ddb8c6983b6e23e98bc713c6988068e83
jwvargas <jwvargas@uce.edu.ec>
2023-06-05 22:08:29 -0500

    Se agrega el proyecto

--- src/main/java/ec/edu/uce/git/srv/Test.java
+++ src/main/java/ec/edu/uce/git/srv/Test.java
@@ -2,7 +2,7 @@ package ec.edu.uce.git.srv;
 
 public class Test {
 
-    public void saludo() {
-        System.out.println("Hola Mundo, mi primer git clone");
+    public void saludo() {System.out.println("Hola Mundo, mi primer git clone");
+        System.out.println("Practica n1");
     }
 }
